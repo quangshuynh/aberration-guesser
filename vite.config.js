06b6211690ea7c9892434110b7da5f473b48c52e
Quang <qth9368@rit.edu>
2025-01-25 12:16:14 -0500

    visualizer & chunksizewarninglimit increase

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
 
   build: {
     outDir: 'build',
+    chunkSizeWarningLimit: 5000
   },
 
   plugins: [react(),
     tailwindcss(),
+    visualizer(),
   ],
 
   base:'/aberration-guesser/',
