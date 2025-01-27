0f320605a704a14fa895b57a1a50553f3527c24f
Quang <qth9368@rit.edu>
2025-01-27 07:31:26 -0500

    footer hyperlink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear(); 
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className="bg-gray-200 text-center py-4 text-sm text-gray-600 w-full">
-      © {currentYear} Aberration Guesser
+      © {currentYear}{" "}
+      <a
+        href="https://www.linkedin.com/in/quangs/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline"
+      >
+        Quang Huynh
+      </a>
     </footer>
   );
 };
